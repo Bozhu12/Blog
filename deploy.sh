660a686130f48a951e81fa55010f2b12beee4bd6
Sanscan12 <1173930245@qq.com>
2023-03-12 08:25:36 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,16 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 初始化
-git clone git@github.com:Sanscan12/Blog.git
-
 # 生成静态文件 / 生成覆盖
 npm run build
 
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
 # 自定义域名
 echo 'blog.bozhu12.cc' > CNAME
+
+
 
 # 部署
 git add -A
